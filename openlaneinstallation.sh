54f9b27f2ec1146866208701a20c2ea5e3f43d98
vision-vlsi <89384474+vision-vlsi@users.noreply.github.com>
2021-09-11 11:35:47 +0530

    Update openlaneinstallation.sh

--- openlaneinstallation.sh
+++ openlaneinstallation.sh
@@ -1,8 +1,36 @@
+echo
+echo
+echo
+echo "            **************************************         "
+echo "             WelCome to Opensource Installation            "
+echo "                   Installation script                     "
+echo
+echo "                         By                                "
+echo
+echo "                 Vision-VLSI Pvt. Ltd.                     "
+echo "              We offer Training in VLSI Domain             "
+echo
+echo "                Contact at +919949426362                   "
+echo "            **************************************         "
+echo
+echo
+echo
+echo "           **************************************          "
+echo "                Installation Begins                        "
+echo "           **************************************          "
+echo
+echo
+echo
 ##Magic Installation
 cd
 cd /var/lib/dpkg/
 sudo rm lock lock-frontend
 cd
+echo
+echo
+echo "Installing Magic dependancies"
+echo
+echo
 sudo apt-get install m4 --assume-yes
 sudo apt-get install tcsh --assume-yes
 sudo apt-get install csh --assume-yes
@@ -13,21 +41,34 @@ sudo apt-get install mesa-common-dev libglu1-mesa-dev --assume-yes
 sudo apt-get install libncurses-dev --assume-yes
 ##sudo apt install git --assume-yes
 cd 
+echo
+echo
+echo "Cloning magic and will start installation of magic"
+echo
+echo
 git clone https://github.com/RTimothyEdwards/magic.git
 cd magic
 ./configure 
 sudo make
 sudo make install
 cd
-
+echo
+echo
 ##yosys – Yosys Open SYnthesis Suite - Installation
-
+echo "Installing yosys dependancies"
+echo
+echo
 sudo apt-get install build-essential clang bison flex \
 	libreadline-dev gawk tcl-dev libffi-dev git \
 	graphviz xdot pkg-config python3 libboost-system-dev \
 	libboost-python-dev libboost-filesystem-dev zlib1g-dev --assume-yes
 	
 cd
+echo
+echo
+echo "       Cloning yosys and will start installation of yosys      "
+echo
+echo
 git clone https://github.com/YosysHQ/yosys.git
 cd yosys
 sudo make
@@ -36,6 +77,11 @@ sudo make install
 
 ##Docker installation
 cd 
+echo
+echo
+echo "                     Installing Docker                        "
+echo
+echo
 sudo apt-get remove docker docker-engine docker.io containerd runc --assume-yes
 sudo apt-get update --assume-yes
 sudo apt-get install \
@@ -53,6 +99,11 @@ sudo apt-get update --assume-yes
 sudo apt-get install docker-ce docker-ce-cli containerd.io --assume-yes
 
 cd 
+echo
+echo
+echo "           Cloning OpenLane and will start installation of openlane             "
+echo
+echo
 git clone https://github.com/The-OpenROAD-Project/OpenLane.git
 cd OpenLane
 sudo make openlane
@@ -64,6 +115,11 @@ sudo make test # This is to test that the flow and the pdk were properly install
     
 ###openSTA installation
 cd 
+echo
+echo
+echo "               Cloning OpenSTA and will start installation of OpenSTA             "
+echo
+echo
 git clone https://github.com/The-OpenROAD-Project/OpenSTA.git
 cd OpenSTA
 mkdir build
@@ -75,10 +131,23 @@ cmake ..
 sudo make
 sudo make install
 cd 
-
+echo
+echo
 ##Install Icarus iverilog
+echo "                                  Installing iverilog                     "
+echo
+echo
 sudo apt-get install -y iverilog --assume-yes
+echo "                                  Installing gtkwave                     "
+echo
+echo
 sudo apt install gtkwave --assume-yes
+echo "                                  Installing klayout                     "
+echo
+echo
 sudo apt install klayout --assume-yes
-
-echo "Installation Completed"
+echo
+echo "                     **************************************             "
+echo "                             Installation Completed                     "
+echo "                     **************************************             "
+echo
